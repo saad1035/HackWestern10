e02d6089ed3c0ed913921b32a2f0cd2a3a455bec
saad1035 <naeemsaad1035@gmail.com>
2023-11-25 15:06:17 -0500

    sn-firstcommit

---
+++ hw10/src/components/Home/Home.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // If you are using React Router
+
+const HomePage = () => {
+    return (
+      <div>
+        {/* Navigation Bar */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/resumeform">Resume Form</Link>
+            </li>
+            {/* Add more navigation links as needed */}
+          </ul>
+        </nav>
+  
+        {/* Main Content Area */}
+        <div>
+          <h1>Welcome to My Website</h1>
+          <p>This is a simple home page template in React.</p>
+  
+          {/* Example Button to Route to Resume Form */}
+          <Link to="/ResumeForm">
+            <button>Go to Resume Form</button>
+          </Link>
+        </div>
+  
+        {/* Footer */}
+        <footer>
+          <p>&copy; 2023 My Website. All rights reserved.</p>
+        </footer>
+      </div>
+    );
+  };
+
+export default HomePage;
