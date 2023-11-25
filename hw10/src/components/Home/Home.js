10b303c5016b1af4feac081f476829d87b44b8a1
Isha Rana <irana4@uwo.ca>
2023-11-25 15:24:19 -0500

    ir-commit

---
+++ hw10/src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="container">
+      <h1>Resumade Easy</h1>
+      <p>
+       Let's build your perfect resume.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
