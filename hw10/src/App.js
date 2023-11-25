f5c88f22e63c3bdc886f6ea8a856ef2f34df7210
saad1035 <naeemsaad1035@gmail.com>
2023-11-25 15:29:00 -0500

    sn-secondcommit

--- hw10/src/App.js
+++ hw10/src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import ResumeForm from './components/ResumeForm/ResumeForm';
+import Home from './components/Home/Home';
+import Contact from './components/Contact/Contact';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path = "/" element = {<Contact/>}/>
+          {/* <Route exact path = "/" element = {<Home/>} />
+          <Route exact path = "/" element = {<ResumeForm />}/> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
