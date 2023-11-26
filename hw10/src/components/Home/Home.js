6279c2467e4efcc82b350ece0c3fd915db663e7c
oomeetka <oomeetka@uwo.ca>
2023-11-25 20:16:15 -0500

    hompage

--- hw10/src/components/Home/Home.js
+++ hw10/src/components/Home/Home.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'; // If you are using React Router
 const HomePage = () => {
     return (
       <div>
+      <div class = "navigation">
         {/* Navigation Bar */}
         <nav>
           <ul>
@@ -19,22 +20,31 @@ const HomePage = () => {
             {/* Add more navigation links as needed */}
           </ul>
         </nav>
+        </div>
   
         {/* Main Content Area */}
         <div class = "header">
           <h1>Welcome to My Website</h1>
           <p>This is a simple home page template in React.</p>
-  
+          </div>
+
+
+          <div class = "intro">
           {/* Example Button to Route to Resume Form */}
           <Link to="/ResumeForm">
             <button>Go to Resume Form</button>
           </Link>
-        </div>
+          </div>
+          
+        
+        
   
         {/* Footer */}
+        <div class = "footer">
         <footer>
           <p>&copy; 2023 My Website. All rights reserved.</p>
         </footer>
+        </div>
       </div>
     );
   };
