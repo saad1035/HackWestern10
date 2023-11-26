8d0c373cb504e19c1970839ea53318d28862759c
oomeetka <oomeetka@uwo.ca>
2023-11-25 20:52:25 -0500

    hompage

--- hw10/src/components/Home/Home.js
+++ hw10/src/components/Home/Home.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'; // If you are using React Router
 const HomePage = () => {
     return (
       <div>
-      <div class = "navigation">
+      
         {/* Navigation Bar */}
         <nav>
           <ul>
@@ -20,7 +20,7 @@ const HomePage = () => {
             {/* Add more navigation links as needed */}
           </ul>
         </nav>
-        </div>
+        
   
         {/* Main Content Area */}
         <div class = "header">
@@ -35,17 +35,29 @@ const HomePage = () => {
             <button>Go to Resume Form</button>
           </Link>
           </div>
-          
+
+          <div class = "fast">
+            <p class = "title">Easy and Fast</p>
+            <p>Our resume builder makes the job application process 10x faster.</p>
+            <p>There are clear and simple step-by-step process to guide users through each section of the resume.</p>
+          </div>
+
+          <div class = "ai">
+            <p class = "title">Use AI</p>
+            <p>Uses AI to generate points based on the job title.</p>
+            <p>Keeps the experience up to date with the current job trends.</p>
+            <p></p>
+          </div>
+
+          <div class = "customizable">
+            <p class = "title">Customizable</p>
+            <p>Easily modify the content within each section to highlight specific achievements, skills, or experience relevant to different job applications. </p>
+          </div>
         
-        
-  
-        {/* Footer */}
-        <div class = "footer">
-        <footer>
-          <p>&copy; 2023 My Website. All rights reserved.</p>
-        </footer>
-        </div>
-      </div>
+          <div class = "footer">
+          <p>2023 My Website. All rights reserved.</p>
+          </div>
+       </div>
     );
   };
 
